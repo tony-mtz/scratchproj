550beecc511fccc96475f9f49b74974355c0bdff
tony <anthony@amartinez.cc>
2021-02-13 15:37:30 -0800

    completed mock pet details

---
+++ client/FakeModel/fakeData.js
@@ -0,0 +1,31 @@
+const data =
+[  
+  {
+    "name": "Archer",
+    "gender": "male",
+    "age": "4",
+    "location": "Long Beach",
+    "about": "Very needy, but awesome!"
+  },
+  {
+    "name": "Jillin",
+    "gender": "female",
+    "age": "4",
+    "location": "Long Beach",
+    "about": "Has own blog and coffee brand."
+
+  },
+  {
+    "name": "Coco",
+    "gender": "male",
+    "age": "2",
+    "location": "Long Beach",
+    "about": "Laker fan!"
+
+  }
+  
+]
+
+export default data;
+
+
--- client/containers/PetContainer.jsx
+++ client/containers/PetContainer.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
 import { Col, Row } from 'react-bootstrap';
-// import PetDetails from '..components/PetDetails';
+import PetDetails from '../components/PetDetails'
 // import PetImages from '..components/PetImages';
 // import SavePet from '..components/SavePet';
+import data from '../FakeModel/fakeData';
 
 
 class PetContainer extends Component {
@@ -12,30 +13,53 @@ class PetContainer extends Component {
     super();
     
     this.state = {
-      urls: []
+      petData: []
     };
   }
 
+  componentDidMount() {  
+    this.setState({petData:data});
+  //replace when we have real data source
+  //   fetch(this.props.feedUrl)
+  //   .then(response=> response.json())
+  //   .then(data=>{
+  //     this.setState({data:petData})//()=>{
+    
+  //   })
+  //   .catch((e)=>{
+  //     console.log('error:',e)
+  //   })
+  }
   render(){
+    console.log(this.state)
+    const dataObjects = this.state.petData.map((obj, i)=>{
+      return(<PetDetails
+      key = {i}
+      name = {obj.name}
+      age = {obj.age}
+      gender = {obj.gender}
+      location = {obj.location}
+      about = {obj.about}
+      />)
+    })
+   
     return(
       <Container>
         <Row className="justify-content-md-center">
-          <Col>(a)</Col>
+        <Col>(a)</Col>
           <Col>(b)
-            <h3>main pet stuff container</h3>
-          {/* <PetImages />
-          <PetDetails feedUrl= {url}/>
-          <SavePet /> */}
-          </Col>
+            <h3>Pets Near You</h3>
+            {dataObjects}
+            </Col>
           <Col>(c)</Col>
        
         </Row>
       </Container>
     );
   }
+  
 }
 
-
-
 // Render an <App> component to the #app div in the body
-ReactDOM.render(<PetContainer />, document.getElementById('app'));
+ReactDOM.render(<PetContainer {...data}/>, document.getElementById('app'));
+export default PetContainer;
